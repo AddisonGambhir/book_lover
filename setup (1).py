38726e5e3198003a519425ecb1e1751b2649fefb
AddisonGambhir <136495646+AddisonGambhir@users.noreply.github.com>
2023-07-31 22:57:11 -0400

    Add files via upload

---
+++ setup (1).py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='BookLover2',
+      version='0.1',
+      description='A simple demonstration package',
+      url='https://github.com/AddisonGambhir/book_lover',
+      author='Addison Gambhir',
+      author_email='ajg7pk@virginia.edu',
+      license='MIT',
+      packages=['my_package'])
